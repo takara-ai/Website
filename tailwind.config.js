afcd4ff9e8de115fe79c613337982a78af558465
jordan-ia <jordan@i-a.cloud>
2023-12-05 15:42:58 +0000

    first oush

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./layouts/**/*.html", "./content/**/*.md"],
+  content: ["./env/layouts/**/*.html", "./env/content/**/*.md"],
   theme: {
     extend: {},
   },
