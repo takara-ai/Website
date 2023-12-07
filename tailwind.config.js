6f8149385e5ee47f5dc4eba55e65ae921ca9ac7c
Jordan Legg <jordan@i-a.dev>
2023-12-07 19:34:44 +0000

    added contact area

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./env/layouts/**/*.html", "./env/content/**/*.md"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "radial-gradient":
+          "radial-gradient(circle at center, var(--tw-gradient-stops))",
+      }),
+      colors: {
+        coral: {
+          darker: "#cd5b45", // or any color you want for the dark center
+          lighter: "#ff7f50", // or any color you want for the light edges
+        },
+      },
+      // If you don't already have a gradientColorStops in your theme, you'll need to add this
+      gradientColorStops: (theme) => ({
+        ...theme("colors"),
+        "coral-darker": "#cd5b45",
+        "coral-lighter": "#ff7f50",
+      }),
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
