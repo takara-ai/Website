82ee56beb2e20fae19f96ca472bfb8420484c9ea
Jordan Legg <jordan@i-a.dev>
2024-01-30 22:12:08 +0000

    made css and js more cacheable

---
+++ env/assets/js/pages/index.js
@@ -0,0 +1,23 @@
+function toggleLanguage() {
+  var japaneseText = document.getElementById("japanese-text");
+  var englishText = document.getElementById("english-text");
+  var isChecked = document.getElementById("language-toggle").checked;
+
+  if (isChecked) {
+    japaneseText.style.display = "none";
+    englishText.style.display = "block";
+  } else {
+    japaneseText.style.display = "block";
+    englishText.style.display = "none";
+  }
+}
+
+const japaneseCharacters = ["仕事", "壮大", "木", "切磋琢磨", "侘寂", "和"]; // Example characters
+
+function changeJapaneseText() {
+  const randomIndex = Math.floor(Math.random() * japaneseCharacters.length);
+  document.getElementById("japanese-char").textContent =
+    japaneseCharacters[randomIndex];
+}
+
+setInterval(changeJapaneseText, 100); // Adjust speed of changing Japanese characters
