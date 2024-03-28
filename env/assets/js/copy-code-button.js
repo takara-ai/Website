213944210fb11d2bb89470037cfeba8455aa7466
Jordan Legg <149483255+404missinglink@users.noreply.github.com>
2024-03-28 12:41:20 +0000

    Refactor copy-code-button.js to improve code structure and add copy button

--- env/assets/js/copy-code-button.js
+++ env/assets/js/copy-code-button.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM fully loaded. Searching for code blocks...");
-
   document.querySelectorAll("pre > code").forEach((codeBlock, index) => {
-    console.log(`Processing code block ${index + 1}`);
+    const pre = codeBlock.parentNode;
+    const wrapper = document.createElement("div");
+    wrapper.style.position = "relative";
+    wrapper.className = "highlight-wrapper"; // You can style this class in your CSS if needed
 
     // Create the copy button
     const button = document.createElement("button");
     button.className =
-      "copy-code-button text-red-700 font-bold py-1 px-2 rounded absolute top-0 right-0 border-dashed border border-black";
-    button.textContent = "Copy";
+      "copy-code-button text-red-700 py-1 px-2 rounded absolute top-0 right-0 -mt-8 text-sm";
+    button.textContent = "Copy Code";
     button.addEventListener("click", () => {
       navigator.clipboard
         .writeText(codeBlock.textContent)
@@ -16,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
           console.log(`Code block ${index + 1} copied.`);
           button.textContent = "Copied!";
           setTimeout(() => {
-            button.textContent = "Copy";
+            button.textContent = "Copy Code";
           }, 2000);
         })
         .catch((err) =>
@@ -24,16 +25,10 @@ document.addEventListener("DOMContentLoaded", () => {
         );
     });
 
-    // Add the button to the pre element (codeBlock's parent)
-    const pre = codeBlock.parentNode;
-    if (pre && pre.style) {
-      pre.style.position = "relative";
-      pre.appendChild(button);
-      console.log(`Button added to code block ${index + 1}`);
-    } else {
-      console.log(
-        `Failed to add button to code block ${index + 1}: No parent node found.`
-      );
+    if (pre.parentNode) {
+      pre.parentNode.insertBefore(wrapper, pre); // Insert the wrapper before the pre element in the DOM
+      wrapper.appendChild(pre); // Move the pre element inside the wrapper
+      wrapper.appendChild(button); // Add the copy button inside the wrapper
     }
   });
 });
